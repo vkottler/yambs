551fa7d1cc3467f0faeadd0b10a909d3fce5e0c6
Vaughn Kottler <vaughnkottler@gmail.com>
2023-07-01 02:01:09 -0500

    1.9.2 - Add 'format-check' target to native

--- yambs/__init__.py
+++ yambs/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.2
-# hash=ee48a542135068482a85830a99af26a4
+# hash=4904eed3d0ad20ef7a26c05a095c6d19
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Yet another meta build-system."
 PKG_NAME = "yambs"
-VERSION = "1.9.1"
+VERSION = "1.9.2"
