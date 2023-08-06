76f72d8ac8580f07fe05658a8ceab9e71cf39cff
Vaughn Kottler <vaughnkottler@gmail.com>
2023-08-06 14:04:47 -0500

    2.4.1 - Fix bug in format checking

--- yambs/__init__.py
+++ yambs/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.3
-# hash=97870f506d5d2f9b5b8e3527f76efa07
+# hash=24e206542fae6896b37aff43eec1c8ec
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Yet another meta build-system."
 PKG_NAME = "yambs"
-VERSION = "2.4.0"
+VERSION = "2.4.1"
