f7525dd2de870a3c2dca682433fff784e9d19f25
Vaughn Kottler <vaughnkottler@gmail.com>
2024-07-04 20:47:45 -0700

    3.0.4 - Make format targets more robust

--- yambs/__init__.py
+++ yambs/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.4
-# hash=1229207e5be7a4167912254256ebd6a2
+# hash=7e05959b0ce8dc1dedf4e30ecad6a07f
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Yet another meta build-system."
 PKG_NAME = "yambs"
-VERSION = "3.0.3"
+VERSION = "3.0.4"
